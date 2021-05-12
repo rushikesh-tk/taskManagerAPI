769557428f244bb1a95586faf0ab285d2b8df9d1
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2021-05-12 23:12:52 +0530

    import dummy data

---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  tasks: [
+    {
+      title: String,
+      content: String,
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
