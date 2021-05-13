ba56afcc3c202aca49cb3c817265e19116dd21a9
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2021-05-13 16:54:14 +0530

    minor change

--- seeder.js
+++ seeder.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import users from "./data/user.js";
 import connectDB from "./config/db.js";
 import User from "./models/userModel.js";
+import Task from './models/taskModel.js'
 
 dotenv.config();
 
@@ -11,11 +12,13 @@ connectDB();
 const importData = async () => {
   try {
     await User.deleteMany();
+    await Task.deleteMany();
     await User.insertMany(users);
-    console.log("Data Imported Successfully");
+    console.log("Data Import : Successful");
     process.exit();
   } catch (error) {
-    console.log(`Error found while importing : ${error}`);
+    console.log("Data Import : Unsuccessful");
+    console.log(`Error : ${error}`);
     process.exit(1);
   }
 };
@@ -23,10 +26,12 @@ const importData = async () => {
 const destroyData = async () => {
   try {
     await User.deleteMany();
-    console.log("Data Destroyed Successfully");
+    await Task.deleteMany();
+    console.log("Data Destruction : Successful");
     process.exit();
   } catch (error) {
-    console.log(`Error found while deleting : ${error}`);
+    console.log("Data Destruction : Unsuccessful");
+    console.log(`Error : ${error}`);
   }
 };
 
