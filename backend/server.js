4c9c3a50d6fe588001691e81cddfcbf66a277bff
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2021-06-09 15:00:46 +0530

    add rate limiter middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 import auth from "./routes/auth.js";
 import task from "./routes/task.js";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 const PORT = 5000;
 
 dotenv.config();
@@ -16,10 +17,17 @@ app.get("/", (req, res) => {
   res.send("Application is running...");
 });
 
+var limiter = new rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5,
+});
+
 app.use(express.json());
+
 app.use(cors());
-app.use(auth);
-app.use(task);
+app.use("/api", limiter);
+app.use("/api", auth);
+app.use("/api", task);
 
 app.use(function (req, res, next) {
   res.setHeader("X-Frame-Options", "SAMEORIGIN");
